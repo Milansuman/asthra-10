5d62661b9f6980de651b7a493f0dbd2ba81af8f4
awindsr <awindsr@gmail.com>
2025-02-19 21:01:59 +0530

    fix(footer): fix footer layout in mobile view

--- components/madeup/footer.tsx
+++ components/madeup/footer.tsx
@@ -1,19 +1,26 @@
-import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react';
+import {
+  Facebook,
+  Github,
+  Instagram,
+  Linkedin,
+  Twitter,
+  Youtube,
+} from 'lucide-react';
 import Image from 'next/image';
 
 export default function Footer() {
   return (
     <footer className="ambit w-full bg-[#0A0A19] font-[350] text-white">
       {/* Top Section */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 divide-y divide-dashed divide-white/50 lg:divide-y-0">
+      <div className="min-h-[95vh] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 divide-y lg:divide-y-0 divide-dashed  ">
         {/* Header Row */}
-        <div className="col-span-1 sm:col-span-2 border-white/50 border-b p-4 sm:p-6 lg:p-8">
-          <h2 className="text-3xl sm:text-4xl md:text-5xl tracking-wide">
+        <div className="col-span-1 sm:col-span-2 border-white/50 border-b p-4 sm:p-6 lg:p-8 flex items-center justify-center font-bold ">
+          <h2 className="text-3xl sm:text-4xl md:text-6xl tracking-wide font-bold">
             Asthra 9.0
           </h2>
         </div>
 
-        <div className="flex items-center justify-center border-white/50 border-b p-4 sm:border-x">
+        <div className="flex items-center justify-center border-b border-white/50  p-4 sm:border-x">
           <Image
             src="/asthra glass.png"
             alt="Asthra Logo"
@@ -22,9 +29,11 @@ export default function Footer() {
             className="object-contain sm:w-[250px] md:w-[300px]"
           />
         </div>
-        <div className="p-4 sm:p-6 lg:p-8 border-white/50 border-b">
+        <div className="p-4 sm:p-6 lg:p-8 border-b border-white/50 ">
           <div className="text-center sm:text-right">
-            <span className="text-4xl sm:text-5xl md:text-6xl">12%</span>
+            <span className="text-xl sm:text-2xl md:text-3xl">
+              Envisioned to explore the possibilities of tomorrow
+            </span>
           </div>
         </div>
 
@@ -36,17 +45,17 @@ export default function Footer() {
                 <h3 className="text-xl sm:text-2xl md:text-3xl mb-4 sm:mb-6">
                   Student Coordinators
                 </h3>
-                <div className="space-y-2 sm:space-y-3 text-gray-400">
-                  <p className="text-sm sm:text-base md:text-lg">
+                <div className="space-y-2 sm:space-y-3 text-[#6E72A0]">
+                  <p className="text-sm sm:text-base md:text-xl">
+                    Rajat Sandeep +91 98461 01882
+                  </p>
+                  <p className="text-sm sm:text-base md:text-xl">
                     Someone 919191919191
                   </p>
-                  <p className="text-sm sm:text-base md:text-lg">
+                  <p className="text-sm sm:text-base md:text-xl">
                     Someone 919191919191
                   </p>
-                  <p className="text-sm sm:text-base md:text-lg">
-                    Someone 919191919191
-                  </p>
-                  <p className="text-sm sm:text-base md:text-lg">
+                  <p className="text-sm sm:text-base md:text-xl">
                     Someone 919191919191
                   </p>
                 </div>
@@ -55,11 +64,11 @@ export default function Footer() {
                 <h3 className="text-xl sm:text-2xl md:text-3xl mb-4 sm:mb-6">
                   Faculty Coordinators
                 </h3>
-                <div className="space-y-2 sm:space-y-3 text-gray-400">
-                  <p className="text-sm sm:text-base md:text-lg">
-                    Someone 919191919191
+                <div className="space-y-2 sm:space-y-3 text-[#6E72A0]">
+                  <p className="text-sm sm:text-base md:text-xl">
+                    Sarju S +91 94472 33663
                   </p>
-                  <p className="text-sm sm:text-base md:text-lg">
+                  <p className="text-sm sm:text-base md:text-xl">
                     Someone 919191919191
                   </p>
                 </div>
@@ -78,23 +87,27 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="hidden lg:block lg:col-span-1">
+        <div className="hidden lg:block lg:col-span-1 row-span-2">
           <div className="h-full grid grid-rows-3">
             <div className="border-b border-white/50 p-8">
               <div className="text-right">
-                <span className="text-4xl sm:text-5xl md:text-6xl">12%</span>
+                <span className="text-xl sm:text-2xl md:text-3xl">
+                  One of the biggest tech fests in Kerala
+                </span>
               </div>
             </div>
             <div className="border-b border-white/50 p-8">
               <div className="text-right">
-                <span className="text-4xl sm:text-5xl md:text-6xl">12%</span>
+                <span className="text-xl sm:text-2xl md:text-3xl">
+                  NATIONAL LEVEL TECHNICAL FEST 2025
+                </span>
               </div>
             </div>
           </div>
         </div>
 
         {/* Bottom Row - Menu Links */}
-        <div className="col-span-1 sm:col-span-2 lg:col-span-2 p-4 sm:p-6 lg:p-8 border-t border-white/50">
+        {/* <div className="col-span-1 sm:col-span-2 lg:col-span-2 p-4 sm:p-6 lg:p-8 border-t border-white/50">
           <div className="grid grid-cols-3 sm:grid-cols-3 gap-6 sm:gap-8">
             <div>
               <h4 className="text-lg sm:text-xl md:text-2xl mb-4 sm:mb-6">
@@ -130,52 +143,52 @@ export default function Footer() {
               </ul>
             </div>
           </div>
-        </div>
+        </div> */}
 
         {/* College Logo */}
-        <div className="p-4 sm:p-6 lg:p-8 flex justify-center items-center">
+        <div className="md:col-span-2 p-4 sm:p-6 lg:p-8 flex justify-center items-center">
           <Image
             src="/sjcet.svg"
             alt="St. Joseph's College Logo"
             width={150}
             height={75}
-            className="object-contain sm:w-[180px] md:w-[200px]"
+            className="object-cover sm:w-[180px] md:w-[300px]"
           />
         </div>
 
         {/* Social Links */}
-        <div className="p-4 sm:p-6 lg:p-8 flex flex-col items-center">
-          <h4 className="text-lg sm:text-xl md:text-2xl mb-4 sm:mb-6">
+        <div className="p-4 sm:p-6 lg:p-8 flex flex-col items-center md:mt-6">
+          <h4 className="text-lg sm:text-xl md:text-3xl mb-4 sm:mb-6">
             Community
           </h4>
           <div className="flex gap-4 justify-center">
             <a
-              href="#"
+              href="https://www.facebook.com/asthra.sjcet/"
               className="p-2 rounded-full bg-gray-800 hover:bg-gray-700 transition-colors"
               aria-label="Facebook"
             >
               <Facebook className="w-4 h-4 sm:w-5 sm:h-5" />
             </a>
             <a
-              href="#"
+              href="https://www.youtube.com/@sjcetpalai/videos"
               className="p-2 rounded-full bg-gray-800 hover:bg-gray-700 transition-colors"
-              aria-label="Twitter"
+              aria-label="Youtube"
             >
-              <Twitter className="w-4 h-4 sm:w-5 sm:h-5" />
+              <Youtube className="w-4 h-4 sm:w-5 sm:h-5" />
             </a>
             <a
-              href="#"
+              href="https://instagram.com/asthra_sjcet"
               className="p-2 rounded-full bg-gray-800 hover:bg-gray-700 transition-colors"
               aria-label="Instagram"
             >
               <Instagram className="w-4 h-4 sm:w-5 sm:h-5" />
             </a>
             <a
-              href="#"
+              href="https://github.com/AsthraSJCET/"
               className="p-2 rounded-full bg-gray-800 hover:bg-gray-700 transition-colors"
-              aria-label="LinkedIn"
+              aria-label="Github"
             >
-              <Linkedin className="w-4 h-4 sm:w-5 sm:h-5" />
+              <Github className="w-4 h-4 sm:w-5 sm:h-5" />
             </a>
           </div>
         </div>
