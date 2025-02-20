a5bb9fe484a04a64aed4cc22fcd1fb91ec373df0
Milansuman <milansuman2016@gmail.com>
2025-02-20 16:08:43 +0530

    feat: add broken rich text editor

---
+++ components/madeup/initialize-mdx-editor.tsx
@@ -0,0 +1,50 @@
+'use client'
+// InitializedMDXEditor.tsx
+import type { ForwardedRef } from 'react'
+import {
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  toolbarPlugin,
+  MDXEditor,
+  UndoRedo,
+  BlockTypeSelect,
+  KitchenSinkToolbar,
+  BoldItalicUnderlineToggles,
+  type MDXEditorMethods,
+  type MDXEditorProps
+} from '@mdxeditor/editor'
+
+// Only import this to the next file
+export default function InitializedMDXEditor({
+  editorRef,
+  ...props
+}: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
+  return (
+    <MDXEditor
+      plugins={[
+        // Example Plugin Usage
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
+        toolbarPlugin({
+          toolbarClassName: "flex flex-row gap-2",
+          toolbarContents(){
+            return <>
+            <UndoRedo/>
+            <BoldItalicUnderlineToggles/>
+            {/* <KitchenSinkToolbar/> */}
+            {/* <BlockTypeSelect/> */}
+            </>
+          }
+        })
+      ]}
+      {...props}
+      ref={editorRef}
+    />
+  )
+}
