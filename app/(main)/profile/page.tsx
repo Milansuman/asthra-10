c8939ccf74e9b16d2647a1918887611bcbb9170d
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2025-02-24 01:53:24 +0530

    feat: profile page demo

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,29 +1,96 @@
-// Adjust path as needed
-import ProfileClient from '@/components/madeup/ProfileClient';
-import type { userZod } from '@/lib/validator';
-import { getServerAuthSession } from '@/server/auth';
-import { redirect } from 'next/navigation';
-import type { z } from 'zod';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from '@/components/ui/button';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableRow
+} from "@/components/ui/table";
+
+import { ChevronRight, Terminal } from "lucide-react";
+
+import Plusbox from '@/components/madeup/box';
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from "@/components/ui/alert";
+import type { EventZodType } from '@/lib/validator';
 
 
-type userSchema = Partial<z.infer<typeof userZod>>;
 
-const sampleUser: userSchema = {
-    email: 'rajatsandeepsen2025@ai.sjcetpalai.ac.in',
-    name: 'Rajat Sandeep Sen',
-    image: '/profileplaceholder.webp',
-    id: '1234567890',
-    college: 'SJCET',
-    department: 'AD',
-};
+export default function ProfilePage() {
+    // const session = await getServerAuthSession();
 
-export default async function ProfilePage() {
-    const session = await getServerAuthSession();
+    // if (!session) {
+    //     redirect('/');
+    // }
 
-    if (!session) {
-        redirect('/');
+    const hasAsthra = false
+    const validProfile = false
+    const listOfEvents: EventZodType[] = []
 
-    }
+    return (
+        <main className='flex flex-col md:flex-row justify-start p-6 min-h-screen ambit'>
+            <Card className='flex-1 flex-col flex'>
+                <CardHeader>
+                    <Avatar className='h-20 w-20'>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <CardTitle>Shadcn (ROLE)</CardTitle>
+                    <CardDescription>shadcn@gmail.com</CardDescription>
+                    <CardDescription>department, year, college</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    {!validProfile && <Alert className='relative text-black'>
+                        <Terminal className="h-4 w-4" />
+                        <AlertTitle>Complete Profile</AlertTitle>
+                        <AlertDescription>
+                            Please complete your profile to purchase Asthra Pass.
+                        </AlertDescription>
+                        <div className='bg-glass-top absolute top-0 left-0 right-0 h-full' />
+                    </Alert>}
+                </CardContent>
+                <CardContent className='flex justify-start flex-row flex-wrap gap-2'>
+                    <Button size={"sm"} variant="glass">Show Profile QR <ChevronRight /></Button>
+                    <Button size={"sm"} variant="glass">400 Credits <ChevronRight /></Button>
+                    <Button size={"sm"} variant="glass">ASTHRA Pass Unlocked <ChevronRight /></Button>
+                    <Button size={"sm"} variant="glass">Show Profile QR <ChevronRight /></Button>
+                    <Button size={"sm"} variant="glass">Show Profile QR <ChevronRight /></Button>
+                </CardContent>
+                {(listOfEvents.length > 0) && <CardContent>
+                    <CardTitle>Purchased Events</CardTitle>
+                    <Plusbox>
+                        <Table>
+                            <TableBody>
+                                {listOfEvents.map((event, i) => (
+                                    <TableRow key={event.id}>
+                                        <TableCell className="font-medium">{i + 1}</TableCell>
+                                        <TableCell>{event.name}</TableCell>
+                                        <TableCell>{"attended"}</TableCell>
+                                        <TableCell className="text-right">
+                                            <Button size={"thin"} variant="glass">View <ChevronRight /></Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </Plusbox>
+                </CardContent>}
+                <CardFooter className='justify-between mt-auto'>
+                    <Button variant="destructive">Edit Profile</Button>
+                    <Button variant="destructive">Sign Out</Button>
+                </CardFooter>
+            </Card>
+            <div className='flex-1'>
 
-    return <ProfileClient session={session} user={sampleUser} />;
+            </div>
+            <div className='flex-1'>
+
+            </div>
+        </main>
+    )
 }
