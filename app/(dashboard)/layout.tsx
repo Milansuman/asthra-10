86695b09887ae65f4c751783576324195f3a187e
Milansuman <milansuman2016@gmail.com>
2025-02-25 23:36:01 +0530

    add dock to dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,31 @@
+import Dock, { type DockItemData } from '@/components/madeup/Dock';
+
+const Items: DockItemData[] = [
+  {
+    link: "/",
+    label: "Home",
+  },
+  {
+    link: "/dashboard",
+    label: "Passes",
+  },
+  {
+    link: "/dashboard/events",
+    label: "Events",
+  },
+  {
+    link: "/dashboard/events/edit",
+    label: "Edit Events",
+  }
+]
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="bg-blue-700 min-h-screen">{children}</div>;
+  return <div className="bg-blue-700 min-h-screen">
+    {children}
+    <Dock items={Items} />
+  </div>;
 }
