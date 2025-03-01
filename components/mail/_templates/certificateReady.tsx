5c8e7e48e72b6871fcb0d7b04eb34750b0ab6729
Aravind Manoj <aravindmng47@gmail.com>
2025-03-01 16:22:43 +0530

    fix: mail footer fix

--- components/mail/_templates/certificateReady.tsx
+++ components/mail/_templates/certificateReady.tsx
@@ -94,23 +94,28 @@ export default function CertificateReadyEmail({ event, user }: CertificateReadyP
                     <Text className="m-0 text-[#50c2ff] text-base font-extrabold">Follow us on:</Text>
                     <Row>
                       <Column>
-                        <Link href="https://sjcetpalai.ac.in">
+                        <Link href="https://asthra.sjcetpalai.ac.in">
                           <Img src={`${baseUrl}/images/social-web.png`} width="25" height="25" alt="Globe" className="object-contain" />
                         </Link>
                       </Column>
                       <Column>
-                        <Link href="https://www.linkedin.com/school/sjcetpalai/">
+                        <Link href="https://www.facebook.com/asthra.sjcet">
+                          <Img src={`${baseUrl}/images/social-facebook.png`} width="25" height="25" alt="Facebook" className="object-contain" />
+                        </Link>
+                      </Column>
+                      <Column>
+                        <Link href="https://instagram.com/asthra_sjcet">
+                          <Img src={`${baseUrl}/images/social-instagram.png`} width="25" height="25" alt="Instagram" className="object-contain" />
+                        </Link>
+                      </Column>
+                      <Column>
+                        <Link href="https://www.linkedin.com/showcase/asthra-sjcet">
                           <Img src={`${baseUrl}/images/social-linkedin.png`} width="25" height="25" alt="LinkedIn" className="object-contain" />
                         </Link>
                       </Column>
                       <Column>
-                        <Link href="https://x.com/SJCET_PALAI">
-                          <Img src={`${baseUrl}/images/social-x.png`} width="25" height="25" alt="X" className="object-contain" />
-                        </Link>
-                      </Column>
-                      <Column>
-                        <Link href="https://www.instagram.com/sjcet_palai/">
-                          <Img src={`${baseUrl}/images/social-instagram.png`} width="25" height="25" alt="Instagram" className="object-contain" />
+                        <Link href="https://whatsapp.com/channel/0029Vb814WN8PgsMKbk1gF0d">
+                          <Img src={`${baseUrl}/images/social-whatsapp.png`} width="25" height="25" alt="Whatsapp" className="object-contain" />
                         </Link>
                       </Column>
                     </Row>
