44d254ffed4c7ed0f8a81b2e94a93517b8b65391
Aravind Manoj <aravindmng47@gmail.com>
2025-03-03 23:24:25 +0530

    feat: event card loading blur

--- components/madeup/event-card.tsx
+++ components/madeup/event-card.tsx
@@ -7,6 +7,7 @@ import Plusbox from "./box";
 import { CheckCircleIcon } from "lucide-react";
 import { type AllDepartments, allDepartments } from "@/logic";
 import { Markdown } from "@/app/_components/md";
+import { useState } from "react";
 
 interface EventCardProps {
 	data: z.infer<typeof eventZod>;
@@ -15,27 +16,37 @@ interface EventCardProps {
 }
 
 const EventCard: React.FC<EventCardProps> = ({ data }) => {
+	const [imageLoading, setImageLoading] = useState(true);
 	return (
 		<Plusbox className="p-2 relative group">
-			<img className="w-full" src={data.poster} alt={data.name ?? "SJCET Events"} />
-			<div className="absolute top-0 left-0 right-0 bottom-0 bg-glass p-4 opacity-0 group-hover:opacity-100 transition-all">
-				<div className="flex flex-col items-center justify-around h-full gap-2">
-					<div className="flex flex-col items-center gap-2">
-						<Label variant={'glass'} className="text-sm font-thin">
-							{data.department === "NA" ? "General Event" : allDepartments[data.department as AllDepartments]}
-						</Label>
-						<h4 className="mt-[4%]">{data.name}</h4>
-						<div className="mt-[4%]">
-							<Markdown>
-								{data.description}
-							</Markdown>
+			<img
+				className={`w-full ${imageLoading ? 'hidden' : 'block'}`}
+				src={data.poster}
+				alt={data.name ?? "SJCET Events"}
+				onLoad={() => setImageLoading(false)}
+			/>
+			{imageLoading ? (
+				<div className="w-full aspect-[3.2/4] bg-glass" />
+			) : (
+				<div className="absolute top-0 left-0 right-0 bottom-0 bg-glass p-4 opacity-0 group-hover:opacity-100 transition-all">
+					<div className="flex flex-col items-center justify-around h-full gap-2">
+						<div className="flex flex-col items-center gap-2">
+							<Label variant={'glass'} className="text-sm font-thin">
+								{data.department === "NA" ? "General Event" : allDepartments[data.department as AllDepartments]}
+							</Label>
+							<h4 className="mt-[4%]">{data.name}</h4>
+							<div className="mt-[4%]">
+								<Markdown>
+									{data.description}
+								</Markdown>
+							</div>
+						</div>
+						<div className="flex gap-2 flex-wrap flex-row items-center justify-center">
+							{data.eventType !== "ASTHRA_PASS_EVENT" && <Label variant={'glass'} className="text-sm font-thin inline-flex gap-1">KTU Points <CheckCircleIcon size={18} /></Label>}
 						</div>
 					</div>
-					<div className="flex gap-2 flex-wrap flex-row items-center justify-center">
-						{data.eventType !== "ASTHRA_PASS_EVENT" && <Label variant={'glass'} className="text-sm font-thin inline-flex gap-1">KTU Points <CheckCircleIcon size={18} /></Label>}
-					</div>
 				</div>
-			</div>
+			)}
 		</Plusbox>
 	);
 };
