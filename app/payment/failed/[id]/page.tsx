1e78ce60d442b9165e240d07b7ee051c1c8bc51b
awindsr <awindsr@gmail.com>
2025-03-04 12:14:22 +0530

    fix: report button

--- app/payment/failed/[id]/page.tsx
+++ app/payment/failed/[id]/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Plusbox from "@/components/madeup/box";
 import { triedAsync } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { getServerAuthSession } from "@/server/auth";
+import ReportButton from "../../_components/ReportButton";
 
 export default async function Page({
   params,
@@ -16,9 +16,6 @@ export default async function Page({
   const { isSuccess, data, error } = await triedAsync(api.sjcetPay.failedPurchase({
     orderId: id,
   }));
-
-  const session = await getServerAuthSession();
-  const userEmail = session?.user?.email || "Not Available";
 
 
 
@@ -106,26 +103,7 @@ export default async function Page({
             <Button link="/profile" variant="glass">
               Back to Profile
             </Button>
-            <Button
-              link={`https://api.whatsapp.com/send?phone=+919846101882&text=${encodeURIComponent(
-                `Hi Team, 
-                   I recently attempted to book my pass for ${data.transaction.eventName}, but my payment didn’t go through. 
-                                           Here are my details for reference:  
-                                           🔹 Event Name: ${data.transaction.eventName}  
-                                           🔹 Event ID: ${data.transaction.eventId}  
-                                           🔹 Username: ${data.transaction.userName}  
-                                           🔹 Email ID: ${userEmail}
-                                           🔹 Amount: ₹${data.transaction.amount}
-                                           🔹 Transaction ID: ${data.transaction.orderId}
-                                           🔹 Order ID: ${data.transaction.orderId}
-                                            
-                   Could you please check this for me at the earliest? I’d love to be a part of the event. Looking forward to your support.  
-                                           Thanks!`
-              )}`}
-              variant="glass"
-            >
-              Report Payment Issue
-            </Button>
+            <ReportButton data={data} />
           </CardFooter>
         </Card>
       </Plusbox>
