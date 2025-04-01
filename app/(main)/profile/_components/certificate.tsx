4c36e49b60fdf02cf935eae0cfad1e84d6b9e25a
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2025-04-02 03:32:10 +0530

    remove secrets

--- app/(main)/profile/_components/certificate.tsx
+++ app/(main)/profile/_components/certificate.tsx
@@ -16,15 +16,15 @@ export const CertificateRender = ({ data }: Props) => {
     const { qrText, userName, eventType, eventName } = data;
 
     const certificateTemplate = {
-        ASTHRA_PASS: `${baseUrl}/co_rgb:000000,l_text:roboto_140_bold_normal_left:${encodeURI(userName)}/fl_layer_apply,y_460/asthra.png`,
-        ASTHRA_PASS_EVENT: `${baseUrl}/co_rgb:000000,l_text:roboto_140_bold_normal_left:${encodeURI(userName)}/fl_layer_apply,y_280/co_rgb:000000,l_text:arial_80_bold_normal_left:${encodeURI(eventName)}/fl_layer_apply,y_540/participation.png`,
-        COMPETITION: `${baseUrl}/co_rgb:000000,l_text:roboto_140_bold_normal_left:${encodeURI(userName)}/fl_layer_apply,y_280/co_rgb:000000,l_text:arial_80_bold_normal_left:${encodeURI(eventName)}/fl_layer_apply,y_540/participation.png`,
-        WORKSHOP: `${baseUrl}/co_rgb:000000,l_text:roboto_140_bold_normal_left:${encodeURI(userName)}/fl_layer_apply,y_280/co_rgb:000000,l_text:arial_80_bold_normal_left:${encodeURI(eventName)}/fl_layer_apply,y_540/participation.png`,
+        ASTHRA_PASS: `${baseUrl}/co_rgb:000000,l_text:roboto_140_bold_normal_left:${encodeURIComponent(userName)}/fl_layer_apply,y_460/asthra.png`,
+        ASTHRA_PASS_EVENT: `${baseUrl}/co_rgb:000000,l_text:roboto_140_bold_normal_left:${encodeURIComponent(userName)}/fl_layer_apply,y_280/co_rgb:000000,l_text:arial_80_bold_normal_left:${encodeURIComponent(eventName)}/fl_layer_apply,y_540/participation.png`,
+        COMPETITION: `${baseUrl}/co_rgb:000000,l_text:roboto_140_bold_normal_left:${encodeURIComponent(userName)}/fl_layer_apply,y_280/co_rgb:000000,l_text:arial_80_bold_normal_left:${encodeURIComponent(eventName)}/fl_layer_apply,y_540/participation.png`,
+        WORKSHOP: `${baseUrl}/co_rgb:000000,l_text:roboto_140_bold_normal_left:${encodeURIComponent(userName)}/fl_layer_apply,y_280/co_rgb:000000,l_text:arial_80_bold_normal_left:${encodeURIComponent(eventName)}/fl_layer_apply,y_540/participation.png`,
     } satisfies Record<EventZodType["eventType"], string>
 
     const imageTemplate = certificateTemplate[eventType];
 
-    const url = `/api/qr/with-image?scale=3&url=${encodeURI(imageTemplate)}&qr=${encodeURI(qrText)}`
+    const url = `/api/qr/with-image?scale=3&url=${encodeURIComponent(imageTemplate)}&qr=${encodeURIComponent(qrText)}`
 
     return (
         <ImageLoader src={url} className='flex-1 min-w-0 object-contain m-0 text-white' />
