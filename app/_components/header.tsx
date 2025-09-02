9c922f6afbe61ca2ca0a3341b7771084b50dfffc
ajayvarma007789 <vaajay322@gmail.com>
2025-09-02 19:18:28 +0530

    fixed header and homepage

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import Image from 'next/image';
+import { Menu } from 'lucide-react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 
 
 export default function Header() {
   return (
-    <nav className="fixed top-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-200px)] h-20 mt-4 bg-transparent flex items-center justify-end md:justify-center  z-[9999]">
-            <div className="hidden md:flex items-center w-full max-w-6xl">
-              {/* Left Logo */}
-              <div className="w-[230px] min-w-[136px] flex items-center justify-center py-2 bg-transparent border border-black rounded-full font-black tracking-widest text-black">
-                <Image src="/asthratext.svg" alt="asthra" width={85.65} height={27.26} className="min-w-[85px] min-h-[27px]" />
-              </div>
-    
-              {/* Connector Line */}
-              <div className="flex items-center -ml-px">
+    <nav className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full md:w-[calc(100%-200px)] h-20 mt-0 md:mt-4 bg-transparent flex items-center justify-end md:justify-center  z-[9999] ">
+      <div className="hidden md:flex items-center w-full max-w-6xl gap-0 md:gap-4 lg:gap-0">
+        {/* Left Logo */}
+        <div className="w-[230px] min-w-[136px] flex items-center justify-center py-2 bg-white border border-black rounded-full font-black tracking-widest text-black">
+          <Image src="/asthratext.svg" alt="asthra" width={85.65} height={27.26} className="min-w-[85px] min-h-[27px]" />
+        </div>
+
+        {/* Connector Line */}
+        {/* <div className="flex items-center -ml-px">
                 <div className="w-8 min-w- h-[2px] bg-gray-600"></div>
                 <div className="w-2 h-2 border-2 border-gray-600 rounded-full -mx-px"></div>
                 <div className="w-12 h-[2px] bg-gray-600 -mx-px"></div>
@@ -25,17 +34,19 @@ export default function Header() {
                 <div className="w-12 h-[2px] bg-gray-600 -mx-px"></div>
                 <div className="w-2 h-2 border-2 border-gray-600 rounded-full -mx-px"></div>
                 <div className="w-8 h-[2px] bg-gray-600"></div>
-              </div>
-    
-              {/* Center Nav Links */}
-              <div className="flex gap-12 justify-center px-8 py-3 bg-[#0B91A6] text-white rounded-full -ml-px -mr-px flex-1">
-                <a href="#Home" className="hover:underline">Home</a>
-                <a href="#About" className="hover:underline">About</a>
-                <a href="#Discover" className="hover:underline">Discover</a>
-              </div>
-    
-              {/* Connector Line */}
-              <div className="flex items-center -ml-px">
+              </div> */}
+
+        <Image src="/assets/navbarline.webp" alt="asthra" width={246.5} height={10} className="min-w-[100px] h-auto hidden lg:block" />
+
+
+        {/* Center Nav Links */}
+        <div className="flex gap-6 md:gap-8 lg:gap-12 justify-center px-8 py-3 bg-[#0B91A6] text-white rounded-full -ml-px -mr-px flex-1 z-[99999] md:text-sm lg:text-base">
+          <a href="#Home" className="hover:scale-110">Home</a>
+          <a href="#About" className="hover:scale-110">About</a>
+          <a href="#Discover" className="hover:scale-110">Discover</a>
+        </div>
+
+        {/* <div className="flex items-center -ml-px">
                 <div className="w-8 h-[2px] bg-gray-600"></div>
                 <div className="w-2 h-2 border-2 border-gray-600 rounded-full -mx-px"></div>
                 <div className="w-12 h-[2px] bg-gray-600 -mx-px"></div>
@@ -47,16 +58,32 @@ export default function Header() {
                 <div className="w-12 h-[2px] bg-gray-600 -mx-px"></div>
                 <div className="w-2 h-2 border-2 border-gray-600 rounded-full -mx-px"></div>
                 <div className="w-8 h-[2px] bg-gray-600"></div>
-              </div>
-    
-              {/* Sign In */}
-              <button className='bg-black text-white font-semibold py-3 rounded-full w-[230px] min-w-[136px]'>
-                Sign In
-              </button>
-            </div>
-            <div className="flex md:hidden px-6">
-              <span className="font-extrabold text-2xl text-black">Menu</span>
-            </div>
-          </nav>
+              </div> */}
+        <Image src="/assets/navbarline.webp" alt="asthra" width={246.5} height={10} className="min-w-[100px] h-auto hidden lg:block" />
+
+
+        {/* Sign In */}
+        <button className='bg-black text-white font-semibold py-3 rounded-full w-[230px] min-w-[100px]'>
+          Sign In
+        </button>
+      </div>
+      <div className="flex md:hidden">
+        <Dialog>
+          <DialogTrigger><Menu className='text-black h-8 w-8 mr-5' /></DialogTrigger>
+          <DialogContent className="rounded-xl">
+            <DialogHeader>
+              <DialogTitle className="text-center">Menu</DialogTitle>
+              <DialogDescription className="mt-8">
+                <div className="flex flex-col gap-6 items-center justify-center py-3  text-black">
+                  <a href="#Home" className="hover:scale-110">Home</a>
+                  <a href="#About" className="hover:scale-110">About</a>
+                  <a href="#Discover" className="hover:scale-110">Discover</a>
+                </div>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </nav>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,121 +2,121 @@ import { heroui } from '@heroui/theme';
 import type { Config } from 'tailwindcss';
 
 const config = {
-  darkMode: ['class'],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-    './node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js',
-  ],
-  prefix: '',
-  theme: {
-  	container: {
-  		center: true,
-  		padding: '2rem',
-  		screens: {
-  			'2xl': '1400px'
-  		}
-  	},
-  	extend: {
-  		colors: {
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			button: {
-  				primary: 'hsl(var(--button-primary))',
-  				secondary: 'hsl(var(--button-secondary))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		cursor: {
-  			default: 'url("/cursor/small.png") 0 0, default',
-  			pointer: 'url("/cursor/point.png") 0 0, pointer',
-  			clicked: 'url("/cursor/clicked.png") 0 0, pointer',
-  			'not-allowed': 'url("/cursor/red.png") 0 0, not-allowed',
-  			text: 'url("/cursor/text.png") 0 0, text',
-  			grab: 'url("/cursor/closed.png") 0 0, grab',
-  			grabbing: 'url("/cursor/open.png") 0 0, grabbing'
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
-  	}
-  },
-  plugins: [
-    require('tailwindcss-animate'),
-    require('tailwindcss-motion'),
-    require('@tailwindcss/container-queries'),
-    require('@tailwindcss/typography'),
-    require('tailwind-corner-smoothing'),
-    require('tailwindcss-bg-patterns'),
-	require('tailwind-scrollbar-hide'),
-    heroui(),
-  ],
+	darkMode: ['class'],
+	content: [
+		'./pages/**/*.{ts,tsx}',
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{ts,tsx}',
+		'./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js',
+	],
+	prefix: '',
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px'
+			}
+		},
+		extend: {
+			colors: {
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				button: {
+					primary: 'hsl(var(--button-primary))',
+					secondary: 'hsl(var(--button-secondary))'
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				sidebar: {
+					DEFAULT: 'hsl(var(--sidebar-background))',
+					foreground: 'hsl(var(--sidebar-foreground))',
+					primary: 'hsl(var(--sidebar-primary))',
+					'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+					accent: 'hsl(var(--sidebar-accent))',
+					'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+					border: 'hsl(var(--sidebar-border))',
+					ring: 'hsl(var(--sidebar-ring))'
+				}
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			cursor: {
+				default: 'url("/cursor/small.png") 0 0, default',
+				pointer: 'url("/cursor/point.png") 0 0, pointer',
+				clicked: 'url("/cursor/clicked.png") 0 0, pointer',
+				'not-allowed': 'url("/cursor/red.png") 0 0, not-allowed',
+				text: 'url("/cursor/text.png") 0 0, text',
+				grab: 'url("/cursor/closed.png") 0 0, grab',
+				grabbing: 'url("/cursor/open.png") 0 0, grabbing'
+			},
+			keyframes: {
+				'accordion-down': {
+					from: {
+						height: '0'
+					},
+					to: {
+						height: 'var(--radix-accordion-content-height)'
+					}
+				},
+				'accordion-up': {
+					from: {
+						height: 'var(--radix-accordion-content-height)'
+					},
+					to: {
+						height: '0'
+					}
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out'
+			}
+		}
+	},
+	plugins: [
+		require('tailwindcss-animate'),
+		require('tailwindcss-motion'),
+		require('@tailwindcss/container-queries'),
+		require('@tailwindcss/typography'),
+		require('tailwind-corner-smoothing'),
+		require('tailwindcss-bg-patterns'),
+		require('tailwind-scrollbar-hide'),
+		heroui(),
+	],
 } satisfies Config;
 
 export default config;
